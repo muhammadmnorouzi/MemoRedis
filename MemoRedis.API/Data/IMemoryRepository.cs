be1458464a27d50895c32ee074c22da0cf121495
Muhammad.M Norouzi <muhammadmnorouzi@gmail.com>
2022-03-24 23:09:36 +0430

    added memory repository abstraction

---
+++ MemoRedis.API/Data/IMemoryRepository.cs
@@ -0,0 +1,11 @@
+using MemoRedis.API.Models;
+
+namespace MemoRedis.API.Data
+{
+    public interface IMemoryRepository
+    {
+        void CreateMemory(Memory memory);
+        Memory GetMemoryById(string id);
+        IEnumerable<Memory> GetAllMemories();
+    }
+}
