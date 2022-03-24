ee256804fd9a7a6ec837e2f07997bb981dce7041
Muhammad.M Norouzi <muhammadmnorouzi@gmail.com>
2022-03-24 22:56:24 +0430

    configured redis db

--- MemoRedis.API/Program.cs
+++ MemoRedis.API/Program.cs
@@ -1,6 +1,10 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IConnectionMultiplexer>(options =>
+    ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("RedisConStr")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
