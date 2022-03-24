1e4201f0a62fe4dc5a401fd6346e7b5b4fe92341
Muhammad.M Norouzi <muhammadmnorouzi@gmail.com>
2022-03-24 23:31:35 +0430

    added controller for Memories

---
+++ MemoRedis.API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemoRedis.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/")]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
---
+++ MemoRedis.API/Controllers/MemoriesController.cs
@@ -0,0 +1,38 @@
+using System;
+using MemoRedis.API.Data;
+using MemoRedis.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemoRedis.API.Controllers
+{
+    public class MemoriesController : BaseApiController
+    {
+        private readonly IMemoryRepository _repo;
+
+        public MemoriesController(IMemoryRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{id}", Name = "RememberById")]
+        public ActionResult<Memory?> RememberById(string id)
+        {
+            Memory? dbMemory = _repo.GetMemoryById(id);
+
+            if (dbMemory is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dbMemory);
+        }
+
+        [HttpPost(Name = "Memorize")]
+        public ActionResult<Memory?> Memorize(Memory memory)
+        {
+            _repo.CreateMemory(memory);
+
+            return CreatedAtRoute(nameof(RememberById), new { Id = memory.Id }, memory);
+        }
+    }
+}
