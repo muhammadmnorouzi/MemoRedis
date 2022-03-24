27ba3acf41beb6875757e668273a42bdc4cf7e5b
Muhammad.M Norouzi <muhammadmnorouzi@gmail.com>
2022-03-24 22:56:10 +0430

    added model Memory

---
+++ MemoRedis.API/Models/Memory.cs
@@ -0,0 +1,9 @@
+namespace MemoRedis.API.Models
+{
+    public class Memory
+    {
+        public string? Name { get; set; }
+        public string? Desctiption { get; set; }
+        public DateTimeOffset Date { get; set; }
+    }
+}
