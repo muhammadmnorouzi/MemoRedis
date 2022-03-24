f7d687294163e81647876241184b8d379fa4f23b
Muhammad.M Norouzi <muhammadmnorouzi@gmail.com>
2022-03-24 23:09:21 +0430

    updated model Memory

--- MemoRedis.API/Models/Memory.cs
+++ MemoRedis.API/Models/Memory.cs
@@ -1,9 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MemoRedis.API.Models
 {
     public class Memory
     {
-        public string? Name { get; set; }
-        public string? Desctiption { get; set; }
-        public DateTimeOffset Date { get; set; }
+        private Memory()
+        {
+
+        }
+
+        public Memory(string desctiption, DateTimeOffset date)
+        {
+            Desctiption = desctiption;
+            _date = date.UtcDateTime;
+        }
+
+
+        [Required]
+        public string Id { get; } = $"memory:{Guid.NewGuid().ToString()}";
+
+        [Required]
+        public string Desctiption { get; } = default!;
+
+        [Required]
+        public DateTimeOffset Date
+        {
+            get => _date;
+        }
+
+        private DateTimeOffset _date;
     }
 }
