abdba9a607fcf1637781ec12846f0e06809e5b4a
Muhammad.M Norouzi <muhammadmnorouzi@gmail.com>
2022-03-25 15:40:24 +0430

    passed , refactored ShouldCreateMemory

--- MemoRedis.Tests.Unit/Data/RedisMemoryRepositoryTests.cs
+++ MemoRedis.Tests.Unit/Data/RedisMemoryRepositoryTests.cs
@@ -1,16 +1,15 @@
 using System;
+using System.Text.Json;
 using MemoRedis.API.Common;
 using MemoRedis.API.Data;
 using MemoRedis.API.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Localization;
 using Moq;
 using StackExchange.Redis;
 using Xunit;
 
 namespace MemoRedis.Tests.Unit.Data
 {
-    public class IMemoryRepositoryTests
+    public class RedisMemoryRepositoryTests
     {
         #region Constants
         const string MemorySetName = "MemorySet";
@@ -23,7 +22,7 @@ namespace MemoRedis.Tests.Unit.Data
         #endregion
 
         #region Ctor
-        public IMemoryRepositoryTests()
+        public RedisMemoryRepositoryTests()
         {
             _redisMock = new Mock<IConnectionMultiplexer>();
             _databaseMock = new Mock<IDatabase>();
@@ -39,24 +38,38 @@ namespace MemoRedis.Tests.Unit.Data
         public void ShouldCreateMemory()
         {
             // Given
+            string id = Memory.CreateId(Guid.NewGuid());
             DateTimeOffset date = DateTimeOffset.UtcNow;
-            Memory memoryToAdd = new Memory("this is description", date);
+            Memory memoryToAdd = new Memory(id, "this is description", date);
             JsonResult<Memory> serializedMemory = memoryToAdd;
 
+            string insertedKey = string.Empty;
+            string insertedValue = string.Empty;
+
             _databaseMock
-                .Setup(x => x
-                    .SetAdd(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), CommandFlags.None))
-                .Returns(true);
+                .Setup(x => x.SetAdd(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), CommandFlags.None))
+                .Callback<RedisKey, RedisValue, CommandFlags>((key, value, _) =>
+              {
+                  insertedKey = key;
+                  insertedValue = value;
+              }).Returns(true);
 
             // When
             _memoryRepository.CreateMemory(memoryToAdd);
 
+
             // Then
             _redisMock.Verify(x => x.GetDatabase(-1, null), Times.Once);
-
             _databaseMock.Verify(x => x.SetAdd(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), CommandFlags.None), Times.Once);
-
             _databaseMock.VerifyNoOtherCalls();
+
+            Assert.Equal(MemorySetName, insertedKey);
+
+            Memory? insertedMemory = JsonSerializer.Deserialize<Memory>(insertedValue);
+
+            Assert.Equal(memoryToAdd.Id, insertedMemory?.Id);
+            Assert.Equal(memoryToAdd.Desctiption, insertedMemory?.Desctiption);
+            Assert.Equal(memoryToAdd.Date, insertedMemory?.Date);
         }
     }
 }
